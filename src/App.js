e8c5f8b6b0d51656eaf29313cc88f51c74ae9aac
samovsisyan <samovsisyan9@gmail.com>
2021-02-15 01:00:36 +0400

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, {Component} from 'react';
-import { BrowserRouter, Route} from 'react-router-dom'
+import {HashRouter, BrowserRouter, Route} from 'react-router-dom'
 import history from './helpers/history';
-
 import Home from "./pages/Home";
 import Shop from "./pages/Shop"
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter history={history}>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/shop" component={Shop}/>
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+            <HashRouter>
+                {/*<BrowserRouter history={history}>*/}
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/shop" component={Shop}/>
+                {/*</BrowserRouter>*/}
+            </HashRouter>
+        );
+    }
 }
 
 export default App;
